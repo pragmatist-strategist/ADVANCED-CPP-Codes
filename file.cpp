2ecc241101c4b7ae9335e238f7e75b002efc22ba
pscse <72128449+pscse@users.noreply.github.com>
2020-10-01 05:01:43 +0530

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -11,6 +11,10 @@ int main(){
     outFile<<1234<<endl;
     outFile.close();
   }
+  
+  /*
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  */
   else{
     cout<<"Could not create file : "<<outputFileName<<endl;
   }
