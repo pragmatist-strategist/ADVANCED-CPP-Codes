336a7652899c35d72777935bcbf6468d1bfc37f0
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-10-01 08:55:38 +0530

    Sum of k smallest elements in BST

---
+++ Sum of k smallest elements in BST.cpp
@@ -0,0 +1,149 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+	Node* temp = new Node;
+	temp->data = val;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+int sum(Node* root, int k);
+
+
+/* 
+Input:
+     10
+    /  \
+   5    11
+  / \ 
+ 4   7
+      \
+       8 , K=2
+
+Output: 9
+Explanation:
+The sum of two smallest elements 
+is 4+5=9.
+
+*/
+
+int main()
+{
+
+	int t;
+	scanf("%d ", &t);
+	while (t--)
+	{
+		string s;
+		getline(cin >> ws, s);
+		int k;
+		cin >> k;
+		Node* root = buildTree(s);
+		cout << sum(root, k) << endl;
+	
+	}
+	return 1;
+}
+
+
+
+
+
+int sum(Node* root, int k) 
+{ 
+  //inorder traversal
+  stack<Node*>s;
+  vector<int>v;
+ int count=0,sum=0;
+ while (count<k)
+    {
+        while (root!=  NULL)
+        {
+            s.push(root);
+            root = root->left;
+        }
+        root = s.top();
+        s.pop();
+        count++;
+        sum+= root->data;
+        root = root->right;
+        if(count==k)
+        return sum;
+    } 
+} 
